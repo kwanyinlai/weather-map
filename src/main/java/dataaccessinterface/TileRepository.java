8a427463c2d5b4c067b3e001e2bf284924eb246f
kwanyinlai <laikwanyin06@gmail.com>
2025-11-06 18:46:23 -0500

    zoo changed from double to int

--- src/main/java/dataaccessinterface/TileRepository.java
+++ src/main/java/dataaccessinterface/TileRepository.java
@@ -5,7 +5,7 @@ import entity.WeatherTile;
 import java.awt.image.BufferedImage;
 
 public interface TileRepository {
-    BufferedImage getTileImageData(int x, int y, double zoom, java.time.Instant timestamp) throws TileNotFoundException;
+    BufferedImage getTileImageData(int x, int y, int zoom, java.time.Instant timestamp) throws TileNotFoundException;
     BufferedImage getTileImageData(WeatherTile tile) throws TileNotFoundException;
     void addTileToCache(WeatherTile tile) throws TileNotFoundException;
 }
