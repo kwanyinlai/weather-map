071b1eebf61df73f03bcc01722dad5d39da77a96
kwanyinlai <laikwanyin06@gmail.com>
2025-11-06 17:09:46 -0500

    decl methods for interface

--- src/main/java/dataaccessinterface/WeatherTileApiFetcher.java
+++ src/main/java/dataaccessinterface/WeatherTileApiFetcher.java
@@ -1,4 +1,17 @@
 package dataaccessinterface;
 
+import entity.WeatherTile;
+
+import java.awt.image.BufferedImage;
+
 public interface WeatherTileApiFetcher {
+
+    /** Make an API call to <a href="https://weathermaps.weatherapi.com">WeatherMaps</a> API to request
+     * image data, and return the image data.
+     *
+     * @param tile the tile for which image data is to be collected for
+     * @return image data associated with <code>tile</code>
+     * @throws TileNotFoundException If image data for <code>tile</code> could not be parsed, or if the tile given is invalid
+     */
+    public BufferedImage getWeatherTileImageData(WeatherTile tile) throws TileNotFoundException;
 }
