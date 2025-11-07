04bee797158f7528aacaa588e9d7aae5f4968693
kwanyinlai <laikwanyin06@gmail.com>
2025-11-07 18:50:35 -0500

    update output boundary documentation

--- src/main/java/interfaceadapter/maptime/ProgramTimeViewModel.java
+++ src/main/java/interfaceadapter/maptime/ProgramTimeViewModel.java
@@ -4,6 +4,7 @@ import interface_adapter.ViewModel;
 import interfaceadapter.maptime.ProgramTimeState;
 
 public class ProgramTimeViewModel extends ViewModel<ProgramTimeState>{
+    public static final String CURRENT_TIME_LABEL = "Current Time:";
 
     public ProgramTimeViewModel() {
         super("time slider");
