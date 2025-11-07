bb5f09c1c889279c84a160f51ff0f507de1492ef
kwanyinlai <laikwanyin06@gmail.com>
2025-11-07 18:49:56 -0500

    presenter documentation and fix property change

--- src/main/java/interfaceadapter/maptime/ProgramTimePresenter.java
+++ src/main/java/interfaceadapter/maptime/ProgramTimePresenter.java
@@ -1,9 +1,11 @@
 package interfaceadapter.maptime;
 
-import usecase.maptime.UpdateMapTimeInputData;
 import usecase.maptime.UpdateMapTimeOutputData;
 import usecase.maptime.UpdateMapTimeOutputBoundary;
 
+/** Presenter class for UpdateMapTime interactor
+ *
+ */
 public class ProgramTimePresenter implements UpdateMapTimeOutputBoundary {
     private final ProgramTimeViewModel programTimeViewModel;
 
@@ -14,6 +16,8 @@ public class ProgramTimePresenter implements UpdateMapTimeOutputBoundary {
     @Override
     public void updateTime(UpdateMapTimeOutputData newTime) {
         // update program time in programTimeViewModel
-        programTimeViewModel.firePropertyChange();
+        ProgramTimeState programTimeState = programTimeViewModel.getState();
+        programTimeState.setTime(newTime.getStamp());
+        programTimeViewModel.firePropertyChange("time slider");
     }
 }
