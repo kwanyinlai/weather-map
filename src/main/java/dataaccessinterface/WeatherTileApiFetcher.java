6f72b3a36da508eccfe5c6ddee295379aedb68bb
kwanyinlai <laikwanyin06@gmail.com>
2025-11-06 22:28:19 -0500

    documentation for WeatherTileApiFetcher

--- src/main/java/dataaccessinterface/WeatherTileApiFetcher.java
+++ src/main/java/dataaccessinterface/WeatherTileApiFetcher.java
@@ -3,6 +3,7 @@ package dataaccessinterface;
 import entity.WeatherTile;
 
 import java.awt.image.BufferedImage;
+import java.util.List;
 
 /** Interface which interacts with the weather API
  *
@@ -17,4 +18,16 @@ public interface WeatherTileApiFetcher {
      * @throws TileNotFoundException If image data for <code>tile</code> could not be parsed, or if the tile given is invalid
      */
     BufferedImage getWeatherTileImageData(WeatherTile tile) throws TileNotFoundException;
+
+
+
+    /** Make an API call to <a href="https://weathermaps.weatherapi.com">WeatherMaps</a> API to request
+     * image data, and return a list of the image data for all the {@link WeatherTile} in the list,
+     * in the same order in which they are given in the parameter list
+     *
+     * @param tiles the list of tile for which image data is to be collected for
+     * @return the list of image data associated with <code>tile</code> given in the same order as given
+     * in <code>tile</code>. Return a null if the image data cannot be found.
+     */
+    List<BufferedImage> getListOfWeatherTileImageData(List<WeatherTile> tiles);
 }
