3332f9c280efd74dc8d0c1d17d4ac23a9c171a0b
kwanyinlai <laikwanyin06@gmail.com>
2025-11-07 18:50:12 -0500

    allow program time label to change on propertychange

--- src/main/java/view/ProgramTimeView.java
+++ src/main/java/view/ProgramTimeView.java
@@ -3,40 +3,53 @@ package view;
 import interfaceadapter.maptime.ProgramTimeController;
 import interfaceadapter.maptime.ProgramTimeState;
 import interfaceadapter.maptime.ProgramTimeViewModel;
-import interfaceadapter.maptime.ProgramTimeController;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
 /**
  * The View for the Program Time slider
  */
-public class ProgramTimeView extends JPanel {
+public class ProgramTimeView extends JPanel implements PropertyChangeListener {
 
     private final String viewName = "program time";
     private final ProgramTimeViewModel programTimeViewModel;
     private ProgramTimeController programTimeController;
     private final JSlider timeSlider;
+    private final JLabel currentTimeTitleLabel;
+    private final JLabel currentTime;
 
     public ProgramTimeView(ProgramTimeViewModel programTimeViewModel) {
         this.programTimeViewModel = programTimeViewModel;
-        timeSlider = new JSlider(SwingConstants.HORIZONTAL);
+        programTimeViewModel.addPropertyChangeListener(this);
+
+        /** Adding JSlider
+         *
+         */
+        timeSlider = new JSlider(SwingConstants.HORIZONTAL, 0, 100, 0);
+        timeSlider.setPreferredSize(new Dimension(500, 50));
         timeSlider.setMajorTickSpacing(1);
         timeSlider.addChangeListener(
                 evt -> {
                     JSlider source = (JSlider) evt.getSource();
                     if (source.getValueIsAdjusting()) {
-                        final ProgramTimeState currentState = programTimeViewModel.getState();
-                        this.programTimeController.execute(currentState);
+                        this.programTimeController.execute(source.getValue());
                     }
                 }
         );
 
+        /** Adding current time label
+         *
+         */
+        currentTimeTitleLabel = new JLabel(ProgramTimeViewModel.CURRENT_TIME_LABEL);
+        currentTimeTitleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        currentTime = new JLabel();
+
         this.add(timeSlider);
+        this.add(currentTimeTitleLabel);
+        this.add(currentTime);
     }
 
     public String getViewName() {
@@ -46,4 +59,14 @@ public class ProgramTimeView extends JPanel {
         this.programTimeController = controller;
     }
 
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        if (evt.getPropertyName().equals("time slider")) {
+            final ProgramTimeState state = (ProgramTimeState) evt.getNewValue();
+            currentTimeTitleLabel.setText(state.getTime().toString());
+        }
+        else{
+            System.out.println(evt.getPropertyName());
+        }
+    }
 }
