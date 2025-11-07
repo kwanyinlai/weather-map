eb58afd73b9840024efba29ffd910a2ce61d1ada
kwanyinlai <laikwanyin06@gmail.com>
2025-11-07 00:53:41 -0500

    added input, output boundary and usecase interactor

---
+++ src/main/java/usecase/maptime/UpdateMapTimeInputBoundary.java
@@ -0,0 +1,4 @@
+package usecase.maptime;
+
+public interface UpdateMapTimeInputBoundary {
+}
---
+++ src/main/java/usecase/maptime/UpdateMapTimeUseCase.java
@@ -0,0 +1,8 @@
+package usecase.maptime;
+
+public class UpdateMapTimeUseCase implements UpdateMapTimeInputBoundary {
+    private ProgramTime programTime;
+    private final ProgramTimeOutputBoundary programTimePresenter;
+
+
+}
