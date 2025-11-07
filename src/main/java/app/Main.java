9dd1c2d480f2a9b31bc34015959f26342214d0fa
kwanyinlai <laikwanyin06@gmail.com>
2025-11-07 13:13:03 -0500

    basic main and app builder which can be extended

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -6,15 +6,7 @@ public class Main {
     public static void main(String[] args) {
         AppBuilder appBuilder = new AppBuilder();
         JFrame application = appBuilder
-                .addLoginView()
-                .addSignupView()
-                .addLoggedInView()
-                .addSignupUseCase()
-                .addLoginUseCase()
-                .addChangePasswordUseCase()
-                .addLogoutUseCase()
                 .build();
-
         application.pack();
         application.setLocationRelativeTo(null);
         application.setVisible(true);
