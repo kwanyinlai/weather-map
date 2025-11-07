c6d98aed5f709bbe97c8ee07a3ded310ad03eb3c
kwanyinlai <laikwanyin06@gmail.com>
2025-11-07 12:57:32 -0500

    create basic view, need main app to test

---
+++ src/main/java/view/ProgramTimeView.java
@@ -0,0 +1,49 @@
+package view;
+
+import interface_adapter.maptime.ProgramTimeController;
+import interface_adapter.maptime.ProgramTimeState;
+import interface_adapter.maptime.ProgramTimeViewModel;
+import interface_adapter.maptime.ProgramTimeController;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+/**
+ * The View for the Program Time slider
+ */
+public class ProgramTimeView extends JPanel {
+
+    private final String viewName = "program time";
+    private final ProgramTimeViewModel programTimeViewModel;
+    private ProgramTimeController programTimeController;
+    private final JSlider timeSlider;
+
+    public ProgramTimeView(ProgramTimeViewModel programTimeViewModel) {
+        this.programTimeViewModel = programTimeViewModel;
+        this.programTimeViewModel.addPropertyChangeListener(this);
+
+
+        timeSlider = new JSlider(SwingConstants.HORIZONTAL);
+        timeSlider.setMajorTickSpacing(1);
+        timeSlider.addChangeListener(
+                evt -> {
+                    JSlider source = (JSlider) evt.getSource();
+                    if (source.getValueIsAdjusting()) {
+                        final ProgramTimeState currentState = ProgramTimeViewModel.getState();
+                        this.programTimeController.execute(currentState);
+                    }
+                }
+        );
+
+        this.add(timeSlider);
+    }
+
+    public String getViewName() {
+        return viewName;
+    }
+
+}
