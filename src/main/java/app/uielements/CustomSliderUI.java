b900582b139a742d2b0800b98dc7e3672b19b9e3
kwanyinlai <laikwanyin06@gmail.com>
2025-11-07 23:54:54 -0500

    improved triangle

--- src/main/java/app/uielements/CustomSliderUI.java
+++ src/main/java/app/uielements/CustomSliderUI.java
@@ -18,16 +18,20 @@ public class CustomSliderUI extends BasicSliderUI {
     public void paintThumb(Graphics g){
         Graphics2D g2d = (Graphics2D) g.create();
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2d.translate(thumbRect.x, thumbRect.y);
-        int arrowWidth = thumbRect.width - thumbRect.x;
-        int arrowHeight = thumbRect.height - thumbRect.y;
-        Polygon triangle = new Polygon();
-        triangle.addPoint(thumbRect.x, thumbRect.y);
-        triangle.addPoint(thumbRect.x - arrowWidth/2, thumbRect.y + arrowHeight);
-        triangle.addPoint(thumbRect.x + arrowWidth/2, thumbRect.y + arrowHeight);
-        g2d.setColor(Color.black);
-        g2d.draw(triangle);
 
+        int arrowWidth = 12;
+        int arrowHeight = 10;
+        int defaultThumbCentreX = thumbRect.x + thumbRect.width / 2;
+        int defaultThumbCentreY = thumbRect.y + thumbRect.height / 2;
+        int arrowPointOffset = defaultThumbCentreY - arrowHeight / 2;
+
+        Polygon triangle = new Polygon();
+        triangle.addPoint(defaultThumbCentreX, defaultThumbCentreY + arrowHeight);
+        triangle.addPoint(defaultThumbCentreX-arrowWidth/2, arrowPointOffset);
+        triangle.addPoint(defaultThumbCentreX + arrowWidth/2, arrowPointOffset);
+        g2d.setColor(Color.black);
+        g2d.fill(triangle);
+        g2d.draw(triangle);
         g2d.dispose();
     }
 }
