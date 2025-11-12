d9987ef1bd0cbdfce33e7330efd06f611c5df765
Ian Lee <eian.lee@mail.utoronto.ca>
2025-11-11 21:19:46 -0500

    added basic propertyChange structure

--- src/main/java/view/DisplayOverlayView.java
+++ src/main/java/view/DisplayOverlayView.java
@@ -1,5 +1,23 @@
 package view;
 
-public class DisplayOverlayView {
-    //TODO
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.Objects;
+
+public class DisplayOverlayView extends JPanel implements PropertyChangeListener {
+
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        if(Objects.equals(evt.getPropertyName(), "size")) {
+            this.setBounds(new Rectangle((Dimension) evt.getNewValue()));
+        } else {
+            //overlay change
+        }
+    }
+
+
 }
