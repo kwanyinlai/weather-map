d380d1a3a747c2736ea7269f5f77008006079d97
gosha003 <igor.tatarov@mail.utoronto.ca>
2025-11-11 20:43:17 -0500

    Minor changes in documentation

--- src/main/java/entity/BookmarkedLocation.java
+++ src/main/java/entity/BookmarkedLocation.java
@@ -1,16 +1,19 @@
 package entity;
 
-import java.time.Instant;
-
 public class BookmarkedLocation extends Location {
     private final String name;
 
-    // General constructor.
+    // Constructor for the BookmarkedLocation.
     public BookmarkedLocation(String name, double latitude, double longitude) {
         super(latitude, longitude);
         this.name = name;
     }
 
+    /**
+     * Returns a {@link this.name} of this BookmarkedLocation.
+     *
+     * @return a {@link this.name}.
+     */
     public String getName() {
         return name;
     }
