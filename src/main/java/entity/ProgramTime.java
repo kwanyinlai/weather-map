34ef2487c43af92e825b5dbfda206753d95036a3
kwanyinlai <laikwanyin06@gmail.com>
2025-11-14 19:12:29 -0500

    correct overflow behaviour — unfixed time slider synchronization, and issue with animation playing and slider is moved, or pausing

--- src/main/java/entity/ProgramTime.java
+++ src/main/java/entity/ProgramTime.java
@@ -9,6 +9,7 @@ public class ProgramTime {
     private java.time.Instant currentTime;
     /** the minimum interval in which program time can be incremented */
     public static final java.time.Duration TIME_INCREMENT = java.time.Duration.ofHours(1);
+    private static final java.time.Duration MAX_FORECAST = java.time.Duration.ofDays(3);
 
     public ProgramTime(java.time.Instant currentTime) {
         this.currentTime = currentTime;
@@ -19,6 +20,9 @@ public class ProgramTime {
      */
     public void incrementTime(){
         currentTime = currentTime.plus(TIME_INCREMENT);
+        if (currentTime.isAfter(java.time.Instant.now().plus(MAX_FORECAST))) {
+            currentTime = java.time.Instant.now();
+        }
     }
 
     /** Increment time by amount of <code>duration</code>
