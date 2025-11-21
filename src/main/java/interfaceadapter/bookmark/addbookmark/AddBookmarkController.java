205585cc4b288f41083d6ebb7e06ce984d87b86c
gosha003 <igor.tatarov@mail.utoronto.ca>
2025-11-20 22:29:13 -0500

    Implemented addBookmark controller and presenter

--- src/main/java/interfaceadapter/bookmark/addbookmark/AddBookmarkController.java
+++ src/main/java/interfaceadapter/bookmark/addbookmark/AddBookmarkController.java
@@ -1,4 +1,43 @@
 package interfaceadapter.bookmark.addbookmark;
 
+import usecase.bookmark.addbookmark.AddBookmarkInputBoundary;
+import usecase.bookmark.addbookmark.AddBookmarkInputData;
+
+/**
+ * Controller for the "add bookmark" flow.
+ * <p>
+ * This class lives in the interface-adapter layer and translates
+ * UI-level operations (e.g., a user clicking an "Add bookmark" button)
+ * into calls to the {@link AddBookmarkInputBoundary} use case.
+ */
 public class AddBookmarkController {
+
+    private final AddBookmarkInputBoundary inputBoundary;
+
+    /**
+     * Constructs a controller that delegates to the given use case input boundary.
+     *
+     * @param inputBoundary the "add bookmark" use case entry point
+     */
+    public AddBookmarkController(AddBookmarkInputBoundary inputBoundary) {
+        this.inputBoundary = inputBoundary;
+    }
+
+    /**
+     * Requests that a new bookmark be added for the specified location.
+     * <p>
+     * Typical usage from the UI layer:
+     * <pre>
+     * addBookmarkController.addBookmark(name, latitude, longitude);
+     * </pre>
+     *
+     * @param name      human-readable name for the bookmark
+     * @param latitude  latitude of the location
+     * @param longitude longitude of the location
+     */
+    public void addBookmark(String name, double latitude, double longitude) {
+        AddBookmarkInputData inputData =
+                new AddBookmarkInputData(name, latitude, longitude);
+        inputBoundary.addBookmark(inputData);
+    }
 }
--- src/main/java/interfaceadapter/bookmark/addbookmark/AddBookmarkPresenter.java
+++ src/main/java/interfaceadapter/bookmark/addbookmark/AddBookmarkPresenter.java
@@ -1,4 +1,68 @@
 package interfaceadapter.bookmark.addbookmark;
 
-public class AddBookmarkPresenter {
+import entity.BookmarkedLocation;
+import interfaceadapter.bookmark.BookmarksViewModel;
+import usecase.bookmark.addbookmark.AddBookmarkOutputBoundary;
+import usecase.bookmark.addbookmark.AddBookmarkOutputData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Presenter for the "add bookmark" use case.
+ * <p>
+ * This class converts the raw output data from the use case into
+ * updates on the {@link BookmarksViewModel}, which the UI listens to.
+ * It does not know anything about Swing or specific UI widgets.
+ */
+public class AddBookmarkPresenter implements AddBookmarkOutputBoundary {
+
+    private final BookmarksViewModel viewModel;
+
+    /**
+     * Creates a presenter that will update the given view model.
+     *
+     * @param viewModel shared view model used by bookmark-related views
+     */
+    public AddBookmarkPresenter(BookmarksViewModel viewModel) {
+        this.viewModel = viewModel;
+    }
+
+    /**
+     * Called by the use case when a bookmark has been successfully added.
+     * <p>
+     * The presenter converts the {@link AddBookmarkOutputData} into a
+     * {@link BookmarkedLocation} and appends it to the list exposed by
+     * the view model.
+     *
+     * @param outputData data describing the newly added bookmark
+     */
+    @Override
+    public void presentAddedBookmark(AddBookmarkOutputData outputData) {
+        BookmarkedLocation newBookmark =
+                new BookmarkedLocation(
+                        outputData.getName(),
+                        outputData.getLatitude(),
+                        outputData.getLongitude()
+                );
+
+        List<BookmarkedLocation> updated = new ArrayList<>(viewModel.getBookmarks());
+        updated.add(newBookmark);
+
+        viewModel.setBookmarks(updated);
+        viewModel.setErrorMessage(null); // clear any previous error
+    }
+
+    /**
+     * Called by the use case when adding a bookmark fails.
+     * <p>
+     * The presenter simply forwards the error message to the view model,
+     * which allows the UI to display it to the user.
+     *
+     * @param errorMessage user-friendly error message
+     */
+    @Override
+    public void presentAddBookmarkFailure(String errorMessage) {
+        viewModel.setErrorMessage(errorMessage);
+    }
 }
