1c8d14cf8fb597e155b4dcf0b23767f89075fbec
gosha003 <igor.tatarov@mail.utoronto.ca>
2025-11-20 22:56:38 -0500

    Removed duplicate commits

--- src/main/java/interfaceadapter/bookmark/listbookmark/ListBookmarksController.java
+++ src/main/java/interfaceadapter/bookmark/listbookmark/ListBookmarksController.java
@@ -1,36 +1,4 @@
 package interfaceadapter.bookmark.listbookmark;
 
-import usecase.bookmark.listbookmark.ListBookmarksInputBoundary;
-import usecase.bookmark.listbookmark.ListBookmarksInputData;
-
-/**
- * Controller for the "list bookmarks" flow.
- * <p>
- * This controller triggers the use case to retrieve all persisted bookmarks.
- * It does not know how the result will be displayed; that is the job of
- * the presenter and view model.
- */
 public class ListBookmarksController {
-
-    private final ListBookmarksInputBoundary inputBoundary;
-
-    /**
-     * Constructs a controller that delegates to the "list bookmarks" use case.
-     *
-     * @param inputBoundary the use case entry point
-     */
-    public ListBookmarksController(ListBookmarksInputBoundary inputBoundary) {
-        this.inputBoundary = inputBoundary;
-    }
-
-    /**
-     * Requests that all current bookmarks be listed.
-     * <p>
-     * Typical usage from the UI layer is to call this on startup or when
-     * the bookmarks view needs to be refreshed.
-     */
-    public void listBookmarks() {
-        ListBookmarksInputData inputData = new ListBookmarksInputData();
-        inputBoundary.listBookmarks(inputData);
-    }
 }
--- src/main/java/interfaceadapter/bookmark/listbookmark/ListBookmarksPresenter.java
+++ src/main/java/interfaceadapter/bookmark/listbookmark/ListBookmarksPresenter.java
@@ -1,48 +1,4 @@
 package interfaceadapter.bookmark.listbookmark;
 
-import interfaceadapter.bookmark.BookmarksViewModel;
-import usecase.bookmark.listbookmark.ListBookmarksOutputBoundary;
-import usecase.bookmark.listbookmark.ListBookmarksOutputData;
-
-/**
- * Presenter for the "list bookmarks" use case.
- * <p>
- * This presenter receives the complete set of bookmarks from the use case
- * and pushes them into the {@link BookmarksViewModel}.
- */
-public class ListBookmarksPresenter implements ListBookmarksOutputBoundary {
-
-    private final BookmarksViewModel viewModel;
-
-    /**
-     * Creates a presenter that will write list results into the given view model.
-     *
-     * @param viewModel shared view model used by bookmark-related views
-     */
-    public ListBookmarksPresenter(BookmarksViewModel viewModel) {
-        this.viewModel = viewModel;
-    }
-
-    /**
-     * Called by the use case when bookmarks have been successfully loaded.
-     *
-     * @param outputData data describing the list of bookmarks to present
-     */
-    @Override
-    public void presentBookmarks(ListBookmarksOutputData outputData) {
-        // The output data already contains a List<BookmarkedLocation>.
-        viewModel.setBookmarks(outputData.getBookmarks());
-        viewModel.setErrorMessage(null);
-    }
-
-    /**
-     * Called when listing bookmarks fails (for example if there is a
-     * persistence error).
-     *
-     * @param errorMessage user-friendly error message
-     */
-    @Override
-    public void presentListBookmarksFailure(String errorMessage) {
-        viewModel.setErrorMessage(errorMessage);
-    }
+public class ListBookmarksPresenter {
 }
