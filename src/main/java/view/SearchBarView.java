bccaa1b7d45d7e30f18587b3a82704bfef9b31c9
junxigary <junxixi.huang@mail.utoronto.ca>
2025-11-25 02:34:00 -0500

    finished the search bar view

---
+++ src/main/java/view/SearchBarView.java
@@ -0,0 +1,62 @@
+package view;
+import entity.LocationWithName;
+import interfaceadapter.searchbar.SearchBarViewModel;
+import interfaceadapter.searchbar.SearchBarController;
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+import java.awt.event.ActionEvent;
+
+public class SearchBarView extends JFrame {
+    private final JTextField searchField = new JTextField(25);
+    private final JList<LocationWithName> resultList = new JList<>();
+    private final SearchBarController controller;
+    private final SearchBarViewModel viewModel;
+
+    public SearchBarView(SearchBarViewModel viewModel, SearchBarController controller) {
+        this.viewModel = viewModel;
+        this.controller = controller;
+        bindEvents();
+        bindViewModel();
+    }
+    private void bindViewModel() {
+        viewModel.addPropertyChangeListener(e -> {
+            if (SearchBarViewModel.SEARCH_RESULTS_PROPERTY.equals(e.getPropertyName())) {
+                List<LocationWithName> newResults = viewModel.getSearchResults();
+                SwingUtilities.invokeLater(() ->
+                        resultList.setListData(newResults.toArray(new LocationWithName[0]))
+                );
+            }
+        });
+    }
+    private void bindEvents() {
+        JButton searchBtn = new JButton("Search");
+        searchBtn.addActionListener(this::onSearchClick);
+
+        JPanel searchPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        searchPanel.add(new JLabel("Search Location："));
+        searchPanel.add(searchField);
+        searchPanel.add(searchBtn);
+
+        JScrollPane resultScroll = new JScrollPane(resultList);
+        resultList.setCellRenderer((list, value,
+                                    index, isSelected, cellHasFocus) -> {
+            JLabel label = new JLabel(value.toString());
+            if (isSelected) {
+                label.setBackground(list.getSelectionBackground());
+                label.setForeground(list.getSelectionForeground());
+                label.setOpaque(true);
+            }
+            return label;
+        });
+
+        setLayout(new BorderLayout(10, 10));
+        add(searchPanel, BorderLayout.NORTH);
+        add(resultScroll, BorderLayout.CENTER);
+    }
+
+    private void onSearchClick(ActionEvent e) {
+        String query = searchField.getText().trim();
+        controller.handleSearch(query);
+    }
+}
