cd10834d78e7bec45daa46d4e5a6c80ef189ed34
kwanyinlai <laikwanyin06@gmail.com>
2025-11-29 16:01:47 -0500

    cast numerator to float in WeatherLayersController.java

--- src/main/java/interfaceadapter/weatherlayers/layers/WeatherLayersController.java
+++ src/main/java/interfaceadapter/weatherlayers/layers/WeatherLayersController.java
@@ -22,7 +22,7 @@ public class WeatherLayersController {
     }
 
     public void executeChangeOpacity(int value){
-        float alpha = (float) (value / Constants.PERCENT_MULTIPLIER);
+        float alpha = ((float) value / Constants.PERCENT_MULTIPLIER);
         opacityInput.change(new ChangeOpacityInputData(alpha));
     }
 }
