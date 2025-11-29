5f447891be69f94a24987b75a24f679a5a843ee8
kwanyinlai <laikwanyin06@gmail.com>
2025-11-29 01:45:14 -0500

    all checkstlye fixes for maptime

--- src/main/java/usecase/maptime/TickMapTimeInputData.java
+++ src/main/java/usecase/maptime/TickMapTimeInputData.java
@@ -1,14 +1,28 @@
 package usecase.maptime;
 
+/** Represents the number of ticks that has passed by
+ *  and thereby the number of ticks to increment.
+ */
 public class TickMapTimeInputData {
-    private int ticks;
-    public TickMapTimeInputData(int ticks){
-        if (ticks <= 0){
-            throw new IllegalArgumentException("Ticks must be non-negative integers.");
+    /**
+     * Number of ticks.
+     */
+    private final int ticks;
+    /**
+     * Creates an input data object for ticks
+     * @param ticks number of ticks
+     */
+    public TickMapTimeInputData(int ticks) {
+        if (ticks <= 0) {
+            throw new IllegalArgumentException(
+                    "Ticks must be non-negative integers."
+            );
         }
         this.ticks = ticks;
     }
-
+    /** Return the number of ticks.
+     * @return number of ticks
+     */
     public int getTicks() {
         return ticks;
     }
